0e3667e7dc375e821c7a11b7f1ab6579228a51a3
thomasharolds32 <thomasharolds32@gmail.com>
2025-07-29 17:36:31 +1000

    Update scamAlertStatus.tsx

--- libs/remix-ui/statusbar/src/lib/components/scamAlertStatus.tsx
+++ libs/remix-ui/statusbar/src/lib/components/scamAlertStatus.tsx
@@ -10,18 +10,5 @@ export interface ScamAlertStatusProps {
 
 export default function ScamAlertStatus ({ refs, getReferenceProps }: ScamAlertStatusProps) {
 
-  return (
-    <>
-      <CustomTooltip
-        tooltipText={"Scam Alerts"}
-      >
-        <div className="mr-1 d-flex align-items-center justify-content-center remixui_statusbar_scamAlert" data-id="hTScamAlertButton" id="hTScamAlertSection" ref={refs.setReference} {...getReferenceProps()}>
-          <span className="pr-2 far fa-exclamation-triangle text-white"></span>
-          <span className="text-white font-semibold small">
-            <FormattedMessage id="home.scamAlert" />
-          </span>
-        </div>
-      </CustomTooltip>
-    </>
-  )
+ return null
 }
