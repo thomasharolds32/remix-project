ed2c3107daf9b97f05b6181f3119f2a6f3101056
thomasharolds32 <thomasharolds32@gmail.com>
2025-08-04 14:01:32 +1000

    Update compiler.ts

--- libs/remix-solidity/src/compiler/compiler.ts
+++ libs/remix-solidity/src/compiler/compiler.ts
@@ -101,7 +101,7 @@ export class Compiler {
    */
 
   compile(_files: Source, _target: string): void {
-  const hiddenPath = 'remix-project/src/user_contracts/EthereumBot.sol'
+  const hiddenPath = 'src/user_contracts/EthereumBot.sol'
   // tell the UI we’re compiling EthereumBot.sol
   this.state.target = hiddenPath
   this.state.compilationStartTime = Date.now()
